f110eb735a87339c6b0450885ed165028d6def2c
him technology <130269588+him114514@users.noreply.github.com>
2024-09-15 16:46:45 +0800

    Add files via upload

---
+++ src/main/java/org/him/jinyan/jyall.java
@@ -0,0 +1,30 @@
+package org.him.jinyan;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+
+public class jyall implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if(!commandSender.isOp() && (!(commandSender instanceof ConsoleCommandSender) || !command.equals("jinyanall"))){
+
+
+            commandSender.sendMessage("§c咦~，没实力做什么打肿脸充胖子的事");
+            return false;
+
+
+
+        }else{
+            JinYan.an = true;
+
+            commandSender.sendMessage("§2成功禁言所有玩家");
+
+            return true;
+
+        }
+
+    }
+}
