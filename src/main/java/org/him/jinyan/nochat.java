35e10e28956857b078053e5815b23fca25d157d4
him technology <130269588+him114514@users.noreply.github.com>
2024-09-17 21:49:46 +0800

    Update nochat.java

--- src/main/java/org/him/jinyan/nochat.java
+++ src/main/java/org/him/jinyan/nochat.java
@@ -9,18 +9,16 @@ public class nochat implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        // 权限检查
+        
         if (!(sender instanceof ConsoleCommandSender) && !sender.isOp()) {
             sender.sendMessage("§c咦~，没实力做什么打肿脸充胖子的事");
             return false;
         }
 
-        // 命令检查
         if (!command.getName().equalsIgnoreCase("jinyan")) {
             return false;
         }
 
-        // 参数检查
         if (args.length == 0) {
             sender.sendMessage("§c语法错误");
             return false;
@@ -29,7 +27,6 @@ public class nochat implements CommandExecutor {
         String playerName = args[0];
         JinYan.noChatPlayers.add(playerName);
 
-        // 更新配置文件
         JinYan.thejinyan.getConfig().set("nochatplayer", JinYan.noChatPlayers);
         JinYan.thejinyan.saveConfig();
 
