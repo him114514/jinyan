97f68b1df80236c041f6b7be5c39b254979a1c62
him technology <130269588+him114514@users.noreply.github.com>
2024-09-17 21:50:10 +0800

    Update jcjyall.java

--- src/main/java/org/him/jinyan/jcjyall.java
+++ src/main/java/org/him/jinyan/jcjyall.java
@@ -9,13 +9,11 @@ public class jcjyall implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        // 权限检查
+        
         if (!(sender.isOp() || sender instanceof ConsoleCommandSender) || !command.getName().equalsIgnoreCase("jiechuall")) {
             sender.sendMessage("§c咦~，没实力做什么打肿脸充胖子的事");
             return false;
         }
-
-        // 解除禁言
         JinYan.thejinyan.getConfig().set("JYALL", false);
         JinYan.thejinyan.getConfig().set("nochatplayer", JinYan.noChatPlayers);
         JinYan.thejinyan.saveConfig();
